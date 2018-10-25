c26db1eef8e535bfc858974eff0595124153cee9
William Johnson <wjohnson777@gmail.com>
2018-10-25 16:14:40 -0500

    Add files via upload

---
+++ js/locations.js
@@ -0,0 +1,11 @@
+// 5 locations hard-coded in the model.
+var locations = [
+  {name: 'Saint Louis ZOO', address: 'Government Dr, St. Louis, MO 63110', location: {lat: 38.63679, lng: -90.29348}},
+  {name: 'Saint Louis Science Center', address: '5050 Oakland Ave, St. Louis, MO 63110', location: {lat: 38.628663, lng: -90.270577}},
+  {name: 'The Gateway Arch', address: ' 50 N Leonor K Sullivan Blvd, St. Louis, MO 63102', location: {lat: 38.624617, lng: -90.185376}},
+  {name: 'Anheuser-Busch', address: '1200 Lynch St, St. Louis, MO 63118', location: {lat:  38.599094, lng: --90.213997}},
+  {name: 'Missouri Botanical Garden', address: '4344 Shaw Blvd, St. Louis, MO 63110',  location: {lat: 38.616105, lng: -90.258067}},
+  {name: 'The Butterfly House', address: '15193 Olive Blvd, Chesterfield, MO 63017', location: {lat: 38.664843, lng: -90.542774}},
+];
+// end locations
+
